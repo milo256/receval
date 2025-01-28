2d04f15480c50cbbd4c7b95f4ee606df68f6b527
milo256 <milobit64@gmail.com>
2025-01-28 11:25:16 -0800

    remove builtin.h

--- receval.c
+++ receval.c
@@ -5,7 +5,6 @@
 
 #include "types.h"
 #include "parse.h"
-#include "builtin.h"
 
 /* my z key is broken. sof = sizeof and b at the end of a
  * variable name means it's a size in (b)ytes */
@@ -16,6 +15,67 @@
 const char DISCARD[TYPE_MAX_SIZE];
 
 void eval_function(Function *, void *, void *, void *);
+void eval_expr(Expr *, void *, void *, void *, void *);
+
+void eval_builtin(
+    OpBuiltin * builtin, void * globals, void * locals, void * outer_args, void * ret_ptr
+) {
+    Expr * args = builtin->args;
+    u32 args_len = builtin->args_len;
+
+    Integer condition;
+    switch (builtin->class) {
+        case B_ADD_I: case B_SUB_I: case B_MUL_I: case B_DIV_I: 
+            ASSERT(args_len > 1);
+            if (args_len == 2) {
+                Integer lhs, rhs;
+                eval_expr(args, globals, locals, outer_args, &lhs);
+                eval_expr(&args[1], globals, locals, outer_args, &rhs);
+                switch (builtin->class) {
+                    case B_ADD_I: *(Integer *)ret_ptr = lhs + rhs; break;
+                    case B_SUB_I: *(Integer *)ret_ptr = lhs - rhs; break;
+                    case B_MUL_I: *(Integer *)ret_ptr = lhs * rhs; break;
+                    case B_DIV_I: *(Integer *)ret_ptr = lhs / rhs; break;
+                    default: PANIC();
+                }
+            } else PANIC();
+            break;
+        case B_IF:
+            ASSERT(args_len > 1 && args_len < 4);
+
+            eval_expr(args, globals, locals, outer_args, &condition);
+            if (condition)
+                eval_expr(&args[1], globals, locals, outer_args, ret_ptr);
+            else if (args_len > 2)
+                eval_expr(&args[2], globals, locals, outer_args, ret_ptr);
+            else
+                memset(ret_ptr, 0, sizeof(Integer));
+            break;
+        case B_WHILE:
+            ASSERT(args_len == 2);
+            
+            eval_expr(args, globals, locals, outer_args, &condition);
+            while (condition) {
+                eval_expr(&args[1], globals, locals, outer_args, ret_ptr);
+                eval_expr(args, globals, locals, outer_args, &condition);
+            }
+            break;
+        case B_SEQ:
+            ASSERT(args_len > 0);
+            for (u32 i = 0; i < args_len - 1; i++) {
+                eval_expr(&args[i], globals, locals, outer_args, ret_ptr);
+            }
+            eval_expr(&args[args_len-1], globals, locals, outer_args, ret_ptr);
+            break;
+        case B_PRINT_I:
+            eval_expr(args, globals, locals, outer_args, ret_ptr);
+            printf("%d\n", *((Integer *) ret_ptr));
+            break;
+            
+        default:PANIC();
+    }
+
+}
 
 /* args is args of containing function. effectively more locals */
 void eval_expr(
